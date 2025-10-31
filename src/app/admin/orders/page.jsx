d2e282ef02df6a094ddbd015a4646bd0f2351912
Parvej101 <you@example.com>
2025-10-31 20:43:47 +0600

    refactor: add dynamic export to OrdersPage component

--- src/app/admin/orders/page.jsx
+++ src/app/admin/orders/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import dbConnect from "../../../../lib/dbConnect";
 import Order from "../../../../models/Order.js";
 import OrderManager from "./components/OrderManager";
